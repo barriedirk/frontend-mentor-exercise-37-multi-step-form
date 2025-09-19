39cc25bfad7200097de0eed2a0f41cd6bb694b06
Barrie Freyre <barriedirk@yahoo.com>
2025-09-18 22:35:26 -0500

    finish styles for the wrapper of the steps and form

--- project/src/app/layouts/main-layout/main-layout.ts
+++ project/src/app/layouts/main-layout/main-layout.ts
@@ -7,8 +7,8 @@ import { Footer } from './components/footer/footer';
   selector: 'app-main-layout',
   imports: [RouterModule, Footer],
   template: `
-    <div class="main_layout flex flex-col justify-center">
-      <main class="main_content flex flex-col items-center justify-center">
+    <div class="main_layout flex min-h-dvh flex-col md:justify-center">
+      <main class="main_content">
         <router-outlet />
       </main>
       <app-footer />
