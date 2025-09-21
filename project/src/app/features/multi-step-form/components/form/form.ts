23bf2ef3766db20712c2c0dedee3ab5b632fffe6
Barrie Freyre <barriedirk@yahoo.com>
2025-09-20 22:34:35 -0500

    add styles for card and switch

--- project/src/app/features/multi-step-form/components/form/form.ts
+++ project/src/app/features/multi-step-form/components/form/form.ts
@@ -1,12 +1,87 @@
-import { ChangeDetectionStrategy, Component, Input, WritableSignal } from '@angular/core';
+import { DecimalPipe } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  Input,
+  QueryList,
+  ViewChildren,
+  WritableSignal,
+} from '@angular/core';
 
 @Component({
   selector: 'app-form',
-  imports: [],
+  imports: [DecimalPipe],
   templateUrl: './form.html',
   styleUrl: './form.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Form {
   @Input() currentStep!: WritableSignal<number>;
+
+  @ViewChildren('cardOption') cardOption!: QueryList<ElementRef<HTMLElement>>;
+
+  plans: {
+    id: string;
+    svg: string;
+    plan: string;
+    pricePerYear: number;
+    duration: string;
+  }[] = [
+    {
+      id: 'plan-arcade',
+      svg: './assets/images/sprite-plan.svg#arcade',
+      plan: 'Arcade',
+      pricePerYear: 90,
+      duration: '2 months free',
+    },
+    {
+      id: 'plan-advanced',
+      svg: './assets/images/sprite-plan.svg#advanced',
+      plan: 'Advanced',
+      pricePerYear: 120,
+      duration: '2 months free',
+    },
+    {
+      id: 'plan-pro',
+      svg: './assets/images/sprite-plan.svg#pro',
+      plan: 'Pro',
+      pricePerYear: 120,
+      duration: '2 months free',
+    },
+  ];
+
+  // @todo, improve
+  keydownCard(event: KeyboardEvent, index: number) {
+    const radios = this.cardOption.toArray();
+
+    if (event.key === 'Enter') {
+      event.preventDefault();
+
+      const currentRadio =
+        radios[index]?.nativeElement.querySelector<HTMLInputElement>('input[type="radio"]');
+
+      if (!currentRadio) return;
+
+      currentRadio.click();
+    }
+
+    if (['ArrowLeft', 'ArrowUp'].includes(event.key)) {
+      event.preventDefault();
+
+      const prev = radios[(index - 1 + radios.length) % radios.length];
+
+      prev.nativeElement.focus();
+      prev.nativeElement.querySelector('input[type="radio"]')?.dispatchEvent(new Event('click'));
+    }
+
+    if (['ArrowRight', 'ArrowDown'].includes(event.key)) {
+      event.preventDefault();
+
+      const next = radios[(index + 1) % radios.length];
+
+      next.nativeElement.focus();
+      next.nativeElement.querySelector('input[type="radio"]')?.dispatchEvent(new Event('click'));
+    }
+  }
 }
