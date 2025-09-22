d4b71caed1ec881ea5d8a71780f99e1f886b9a21
Barrie Freyre <barriedirk@yahoo.com>
2025-09-21 22:13:06 -0500

    add styles for step 3

--- project/src/app/features/multi-step-form/components/form/form.ts
+++ project/src/app/features/multi-step-form/components/form/form.ts
@@ -51,6 +51,36 @@ export class Form {
     },
   ];
 
+  addOns: {
+    id: string;
+    label: string;
+    value: string;
+    information: string;
+    price: number;
+  }[] = [
+    {
+      id: 'online-services',
+      label: 'Online service',
+      value: 'Online service',
+      information: 'Access to multiplayer games',
+      price: 1,
+    },
+    {
+      id: 'larger-storage',
+      label: 'Larger Storage',
+      value: 'Larger Storage',
+      information: 'Extra 1TB of cloud save',
+      price: 2,
+    },
+    {
+      id: 'customizable-profile',
+      label: 'Customizable profile',
+      value: 'Customizable profile',
+      information: 'Custom theme on your profile',
+      price: 2,
+    },
+  ];
+
   // @todo, improve
   keydownCard(event: KeyboardEvent, index: number) {
     const radios = this.cardOption.toArray();
